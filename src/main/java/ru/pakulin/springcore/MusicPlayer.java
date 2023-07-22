126c2493d56598c3f7520c943161b4da40155126
lewe0fun <pakuln.yury@gmail.com>
2023-07-22 23:30:30 +0300

    6 урок

--- src/main/java/ru/pakulin/springcore/MusicPlayer.java
+++ src/main/java/ru/pakulin/springcore/MusicPlayer.java
@@ -1,12 +1,44 @@
 package ru.pakulin.springcore;
 
 public class MusicPlayer {
-    private Music music;
 
-    public MusicPlayer(Music music) {
+    /*
+     * Класс музыкального плеера
+     */
+    private Music music;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    /*
+     * @param music - зависимость извне
+     */
+
+/*    public MusicPlayer(Music music) {
+        this.music = music;
+    }*/
+
+    //инверсия зависимостей
+
+    public void setMusic(Music music) {
         this.music = music;
     }
-    public void playMusic(){
+
+    public void playMusic() {
         System.out.println(music.getSong());
     }
 }
--- src/main/java/ru/pakulin/springcore/testSpring.java
+++ src/main/java/ru/pakulin/springcore/testSpring.java
@@ -4,11 +4,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class testSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context =new ClassPathXmlApplicationContext(
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        Music testBean = context.getBean("MusicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(testBean);
+        //Music testBean = context.getBean("MusicBean", Music.class);
+        //MusicPlayer musicPlayer = new MusicPlayer(testBean);
+
+        /*
+         *Dependency injection
+         */
+
+        MusicPlayer musicPlayer = context.getBean("MusicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName()+"\n"+musicPlayer.getVolume());
+        context.close();
     }
 }
