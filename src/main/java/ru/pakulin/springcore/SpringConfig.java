e6f325ce1487cc166d72569d1ce250fe67545668
lewe0fun <pakuln.yury@gmail.com>
2023-07-24 21:19:29 +0300

    java + аннотации

--- src/main/java/ru/pakulin/springcore/SpringConfig.java
+++ src/main/java/ru/pakulin/springcore/SpringConfig.java
@@ -1,4 +1,12 @@
 package ru.pakulin.springcore;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("ru.pakulin.springcore")
+@PropertySource("classpath:musicPlayer.properties")
 public class SpringConfig {
+
 }
--- src/main/java/ru/pakulin/springcore/testSpring.java
+++ src/main/java/ru/pakulin/springcore/testSpring.java
@@ -1,14 +1,15 @@
 package ru.pakulin.springcore;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class testSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml");
+        //ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        //musicPlayer.playMusic(Genre.ELECTRONIC);
+        musicPlayer.playMusic(Genre.ELECTRONIC);
         System.out.println(musicPlayer.getName()+" ~ "+musicPlayer.getVolume());
 
         context.close();
